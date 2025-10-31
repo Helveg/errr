9a3169af863ea749579a099a5e067c197ded9a19
Robin De Schepper <robin.deschepper93@gmail.com>
2025-10-31 14:27:56 +0100

    Bump version from 1.1.2 to 1.1.3

--- errr/__init__.py
+++ errr/__init__.py
@@ -1,7 +1,7 @@
 from .exception import DetailedException
 from .tree import make_tree, exception
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 
 def wrap(err_type, *args, **kwargs):
     return err_type.wrap(*args, **kwargs)

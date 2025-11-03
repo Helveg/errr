14c8d5a9ecc7d8b14e4fd2290603c2adc9849f41
Dimitri RODARIE <d.rodarie@gmail.com>
2025-11-03 17:05:54 +0100

    fix: add missing quotejoin function (#6)

--- errr/__init__.py
+++ errr/__init__.py
@@ -9,3 +9,7 @@ __version__ = "1.2.1"
 
 def wrap(err_type, *args, **kwargs):
     return err_type.wrap(*args, **kwargs)
+
+
+def quotejoin(itr, delim=", ", quote="'", f=str):
+    return delim.join(f"{quote}{f(o)}{quote}" for o in itr)

b2c805dde0b41fe3d4bb38ff4a4bca49f1e617c5
Robin De Schepper <robin.deschepper93@gmail.com>
2025-11-03 14:26:37 +0100

    v1.2.1 - tblib 3.2compatibility

--- errr/__init__.py
+++ errr/__init__.py
@@ -1,7 +1,7 @@
 from .exception import DetailedException
 from .tree import make_tree, exception
 
-__version__ = "1.1.3"
+__version__ = "1.2.1"
 
 def wrap(err_type, *args, **kwargs):
     return err_type.wrap(*args, **kwargs)

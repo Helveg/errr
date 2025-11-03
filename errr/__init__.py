cd828379a352de0ed6fed8ac8f64072cf3402e54
Robin De Schepper <robin.deschepper93@gmail.com>
2025-11-03 17:06:46 +0100

    v1.2.2 - reintroduced quotejoin

--- errr/__init__.py
+++ errr/__init__.py
@@ -5,7 +5,7 @@ Elegant detailed Python exceptions.
 from .exception import DetailedException
 from .tree import make_tree, exception
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 def wrap(err_type, *args, **kwargs):
     return err_type.wrap(*args, **kwargs)
